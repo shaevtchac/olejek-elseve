8c774148704e24a7c426ea38cc060619cb97bed6
shaevtchac <shaevtchac@gmail.com>
2023-06-20 16:14:05 +0200

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+// _______________________________________________________________ animations _______________________________________________________________
+
+const animatedElements = document.querySelectorAll('[data-animation]');
+
+const animationObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const {
+      animation,
+      duration = '500ms',
+      'timing-function': timingFunction = 'ease-in',
+      delay = '500ms',
+      'iteration-count': iterationCount = '1',
+      direction = 'normal',
+      'fill-mode': fillMode = 'backwards',
+    } = entry.target.dataset;
+    if (entry.isIntersecting) {
+      entry.target.style.animation = `${animation} ${duration} ${timingFunction} ${delay} ${iterationCount} ${direction} ${fillMode}`;
+    }
+  });
+});
+
+animatedElements.forEach((element) => {
+  animationObserver.observe(element);
+});
